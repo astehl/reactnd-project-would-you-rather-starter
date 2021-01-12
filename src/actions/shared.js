8455632f2017029e66c70bd280abce2a262cde39
Andreas Stehl <andreas.stehl@ista.com>
2021-01-12 07:51:55 +0100

    feat: extend load initial data - set authed user - use loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,18 @@
 import {loadInitialData} from "../utils/api";
 import {loadUsers} from "./users";
 import {loadQuestions} from "./questions";
+import {hideLoading, showLoading} from "react-redux-loading";
+import {setAuthedUser} from "./authedUser";
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading());
+        dispatch(setAuthedUser('sarahedo'))
         loadInitialData()
             .then(({users, questions}) => {
                 dispatch(loadUsers(users));
                 dispatch(loadQuestions(questions));
+                dispatch(hideLoading());
             })
             .catch((e) => {
                 console.warn('error while loading initial data', e);
