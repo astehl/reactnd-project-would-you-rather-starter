e8ef179516cd0134afafcdd9821e5b781be45d3b
Andreas Stehl <andreas.stehl@ista.com>
2021-02-02 10:24:46 +0100

    feat: added avatar icon for authed user in nav-bar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,13 @@
 export default function Avatar(props) {
 
-    const {user} = props;
+    const {user, type} = props;
 
+    const className = (type === 'small') ? 'avatar-small' : 'avatar';
     return (
         <img
             src={process.env.PUBLIC_URL + user.avatarURL}
             alt={`Avatar of ${user.name}`}
-            className='avatar'
+            className={className}
         />
     )
 }
