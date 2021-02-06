a5610fc779fca5feb2cc822466485665013901ec
Andreas Stehl <andreas.stehl@ista.com>
2021-02-06 19:11:03 +0100

    cleanup

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function getCurrentUserAnswer(question, user) {
-    if (question.optionOne.votes.includes(user)) {
-        return 'optionOne';
-    }
-    if (question.optionTwo.votes.includes(user)) {
-        return 'optionTwo';
-    }
-    return '';
-}
